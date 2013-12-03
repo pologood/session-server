d8a54b4383d3a7e06e031fc6162810663ad7f64c
hujunfei <hujunfei@sogou-inc.com>
2013-12-03 17:43:43 +0800

    增加passport-session-sdk测试用例

---
+++ passport-session-web/src/test/java/com/sogou/upd/passport/BaseTest.java
@@ -0,0 +1,18 @@
+package com.sogou.upd.passport;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: shipengzhi
+ * Date: 13-4-23
+ * Time: 上午1:19
+ * To change this template use File | Settings | File Templates.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-config-test.xml")
+public class BaseTest {
+
+}
